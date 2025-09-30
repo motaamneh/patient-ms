47396bd51c67aa56a371b9d95cb8167e64261d31
motaamneh <mtaamneh292@gmail.com>
2025-09-30 16:34:40 +0300

    Initial commit: setup patient-service microservice

---
+++ patient-service/src/test/java/com/motaamneh/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motaamneh.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
